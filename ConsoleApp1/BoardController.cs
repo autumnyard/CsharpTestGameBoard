b07edd675279c3121af9ca19102904b8fbd07284
autumnyard <autumnyardworks@gmail.com>
2023-04-06 21:09:07 +0200

    Add project files.

---
+++ ConsoleApp1/BoardController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class BoardController
+    {
+        private int[] _board;
+        private int _playerPosition;
+
+        public int[] Board => _board;
+
+        public BoardController(int size, int spawnPosition)
+        {
+            if (size < spawnPosition)
+                spawnPosition = size;
+
+            _board = new int[size];
+            _board[0] = 0;
+
+            _playerPosition = spawnPosition;
+        }
+
+        public void Move(eMovement movement)
+        {
+            switch (movement)
+            {
+                case eMovement.Left:
+                    if (_playerPosition == 0) return;
+                    return;
+
+                case eMovement.Right:
+                    if (_playerPosition == _board.Length - 1) return;
+                    break;
+            }
+
+        }
+
+        public void Print()
+        {
+            StringBuilder sb = new StringBuilder("Board: ");
+            for (int i = 0; i < _board.Length; i++)
+            {
+                sb.Append($" [{_board[i]}]");
+            }
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static int Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            BoardController board = new BoardController(3, 2);
+            board.Print();
+
+            bool isPlaying = true;
+
+            while (isPlaying)
+            {
+                var key = Console.ReadKey();
+                switch (key.Key)
+                {
+                    case ConsoleKey.Escape:
+                        isPlaying = false;
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+
+
+            return 0;
+        }
+    }
+}
